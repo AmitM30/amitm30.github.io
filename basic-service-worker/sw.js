62f3856569b7a0bd17f6df349f2a07c891987975
Amit M <mangalamitash@gmail.com>
2017-04-14 17:06:47 +0530

    sw.js path refs fixed

--- basic-service-worker/sw.js
+++ basic-service-worker/sw.js
@@ -4,17 +4,17 @@ var self                      = this;
 var internals                 = {};
 internals.LRUCache            = {};             // Object to maintain timestamp for request urls
 
-internals.STATIC_CACHE_NAME   = 'static-v1-4ryhq1m';
-internals.API_CACHE_NAME      = 'api-v1-4ryhq1m';
-internals.SUGGEST_CACHE_NAME  = 'suggest-v1-4ryhq1m';
+internals.STATIC_CACHE_NAME   = 'static-v1-lfgjpsc';
+internals.API_CACHE_NAME      = 'api-v1-lfgjpsc';
+internals.SUGGEST_CACHE_NAME  = 'suggest-v1-lfgjpsc';
 
 internals.SUGGEST_CACHE_TIME  = 60 * 5 * 1000;  // in seconds
 internals.API_CACHE_TIME      = 60 * 5 * 1000;  // in seconds
 
 internals.CACHE_FILE_LIST = [
-  '/fonts/proximanova-regular-webfont.woff2',
-  '/styles/site.css',
-  '/images/iron_man.jpg'
+  '../fonts/proximanova-regular-webfont.woff2',
+  'styles/site.css',
+  'images/iron_man.jpg'
 ];
 
 internals.CACHE_API_LIST = [
